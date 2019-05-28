6bd5d6247d5f38046ea47ade79c21997fd63af75
Nick Ruggieri <nicolasruggieri7@gmail.com>
2019-05-28 15:11:36 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,56 @@
 # your code goes here
+require "pry"
+
+def begins_with_r(array)
+  condition = true
+  array.each do |element|
+    condition = false if element[0] != "r"
+  end
+  condition
+end
+
+def contain_a(array)
+  collection = []
+  array.each do |element|
+    collection << element if element.include?("a")
+  end
+  collection
+end
+
+def first_wa(array)
+  first_wa = nil
+  array.each do |element|
+    if element.match("wa")
+      first_wa = element
+      break
+    end
+  end
+  first_wa
+end
+
+def remove_non_strings(array)
+  collection = []
+  array.each do |element|
+    collection << element if element.is_a?(String)
+  end
+  collection
+end
+
+def count_elements(array)
+  array.each do |elements_hash|
+    #binding.pry
+   elements_hash[:count] = 0
+   #binding.pry
+   name = elements_hash[:name]
+   array.each do |new_hash|
+     #binding.pry
+     if new_hash[:name] == name
+       elements_hash[:count] += 1
+     end
+   end
+ end.uniq
+end
+
+def merge_data(keys, data)
+
+end
